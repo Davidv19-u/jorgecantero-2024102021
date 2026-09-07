b9b42cf82f624a503a0bc915ece37b459ef70dfc
Laboratorio Asu <laboratorioasu@unida.edu.py>
2026-09-07 20:31:02 -0300

    first commit

---
+++ ejer1/src/main/java/com/mycompany/ejer1/Ejer1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejer1;
+
+/**
+ *
+ * @author laboratorioasu
+ */
+public class Ejer1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
